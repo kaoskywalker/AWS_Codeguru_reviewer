aa95e5db7c11a71b629b3f909d6ac35373249777
kaoskywalker <74879154+kaoskywalker@users.noreply.github.com>
2025-05-09 01:50:47 +0700

    Update and rename Injection to Injection.py

--- Injection.py
+++ Injection.py
@@ -3,7 +3,7 @@ import sqlite3
 def get_user_data(username):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    # ❌ SQL Injection vulnerability
+
     query = f"SELECT * FROM users WHERE username = '{username}'"
     cursor.execute(query)
     result = cursor.fetchall()
